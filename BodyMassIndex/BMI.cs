6581acb16fde4ff911d2995990070640c702e08a
DaimlerFOX <aleksandrchernyx3@gmail.com>
2021-06-22 22:08:05 +0300

    Добавьте файлы проекта.

---
+++ BodyMassIndex/BMI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BodyMassIndex
+{
+    class BMI
+    {
+        public double HumanHeight { get; set; }
+        public double HumanWeight { get; set; }
+
+        public BMI(double humanHeight, double humanWeight)
+        {
+            if (humanHeight>0)
+            {
+                HumanHeight = humanHeight;
+            }
+            else
+            {
+                throw new ArgumentException("Аргумент не может быть меньше 0");
+            }
+
+            if (humanHeight > 0)
+            {
+                HumanWeight = humanWeight;
+            }
+            else
+            {
+                throw new ArgumentException("Аргумент не может быть меньше 0");
+            }
+        }
+
+        private double СalculationBodyMassIndex()
+        {
+            double bmi = HumanWeight / Math.Pow((HumanHeight / 100),2);
+            return bmi;
+        }
+
+        private static string DescriptionBodyMassIndex(double bmi)
+        {
+            if (bmi < 18.5)
+            {
+                return "Ниже нормального";
+
+            }else if (bmi>=18.5 && bmi<25)
+            {
+                return "Норма";
+            }
+            return "Выше нормального";
+        }
+
+        public override string ToString()
+        {
+            double bmi = СalculationBodyMassIndex();
+            string descriptiondmi = DescriptionBodyMassIndex(bmi);
+            return $"ИМТ={Math.Round(bmi,2)} - {descriptiondmi}";
+        }
+    }
+}
---
+++ BodyMassIndex/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BodyMassIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите ваш рост в см.");
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите ваш вес в кг.");
+                var weight = Convert.ToDouble(Console.ReadLine());
+
+                var human = new BMI(height, weight);
+                Console.WriteLine(human.ToString());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Были введены неправильные данные!");
+            }
+          
+        }
+    }
+}
